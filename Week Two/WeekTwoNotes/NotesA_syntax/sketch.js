f8074c8a06e7b9ba7f7a7c0f5a440efde425e66c
aea67 <aea67@miami.edu>
2017-01-26 18:32:22 -0500

    Notes week 2

---
+++ Week Two/WeekTwoNotes/NotesA_syntax/sketch.js
@@ -0,0 +1,45 @@
+//this is how you make invsible comment to yourself or other programmers
+//the above function is not going to work
+
+/* 
+this
+is an
+invisible
+multi line
+comment
+lookin like a 
+poem 
+*/
+
+// Global Variable Declarations
+var num = 100;
+var num2 = 50;
+var num3 = 1000;
+var zeven = 7;
+var message = "Hello this is the first message decleared by var. It should print out.";
+var message2 = "Goodbye. You have reached the end of the program";
+var booleanVar = true;
+var booleanVar = false;
+
+function setup() {
+console.log("Number 1 is " + (num));
+console.log("Number 2 is " + (num2));
+console.log ("Number 1  + Number 2 = " + (num + num2));
+console.log("Number 1 * Number 2 = " + (num*num2));
+console.log(message);
+console.log(message2);
+console.log(message * num3);            
+
+
+//Variable Declarations in Setup Function
+    
+var messsage3 = "This function only works in setup" ;
+console.log(message3);
+
+    
+}
+
+
+function draw() {
+console.log(num)
+}
