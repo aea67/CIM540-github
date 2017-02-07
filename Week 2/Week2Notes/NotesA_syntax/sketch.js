a3ca4dbe3f6de0cab5622615021beb07dd6a4ba8
aea67 <aea67@miami.edu>
2017-02-07 10:36:16 -0500

    more commentary
    Added more explanations and descriptions to week2notes- notes_syntax

--- Week 2/Week2Notes/NotesA_syntax/sketch.js
+++ Week 2/Week2Notes/NotesA_syntax/sketch.js
@@ -1,5 +1,4 @@
 //this is how you make invsible comment to yourself or other programmers
-//the above function is not going to work
 
 /* 
 this
@@ -11,7 +10,27 @@ lookin like a
 poem 
 */
 
-// Global Variable Declarations
+//Variables, aka var
+/* establishing the values for the variables you are creating. 
+These variables will be used intthe program you are writing to carry out specific tasks.
+For instance
+the value of the "num" variable is 100. 
+"var" means variable. Name it whatever you like. You usually name it what it corresponds to.
+A number variable corresponds to number values. 
+boolean variables correspond to two values, true or false.
+word variables correspond to whatever message you want to have print out.
+//Global variables
+declared outside of the function, they are global because they can be used anywhere in the program in any functions.
+//Functions
+function inside does a certain task. 
+script inside competes a certain ask. function 
+For instance
+function setup() means 
+console.log()
+Within those parantheses goes the variables.
+*/
+
+//Global Variable Declarations
 var num = 100;
 var num2 = 50;
 var num3 = 1000;
@@ -45,6 +64,7 @@ console.log(num)
 /* What is the purpose of this exercise?
 I don't know. 
 But Anyways
+The following text is a summary of what I've learned:
 None of what you see here will be printed out onto the browser's web page. 
 Clicking index.html from this folder, "Week2Notes_syntax", the windeow in the browser will be empty.
 In order to see what all this coding in this file, "sketch.js" is actually doing follow these instructions:
