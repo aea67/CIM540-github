b868e23a9e8b1a0033fdf8ab41c8ad01c318b21f
aea67 <aea67@miami.edu>
2017-03-09 13:22:51 -0500

    +midtermfolder +airguitarfile
    made a midterm folder and added air guitar file

---
+++ MIDTERM/AirGuitar/sketch.js
@@ -0,0 +1,14 @@
+function setup() 
+{ 
+
+}
+
+function draw() 
+{ 
+  
+}
+
+/*
+This is an empty file in an folder that is to be used whenever you create a new java script program.
+Duplicate it whenever you want a new program so you have all of the libraries, a file and the index ready to go. 
+*/
